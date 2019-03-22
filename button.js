a0eae578590225672df2c13e6a45a887a8c39d61
ColgateLeoAscenzi <lascenzi@colgate.edu>
2019-03-21 23:30:02 -0400

    removed bulky alert

--- button.js
+++ button.js
@@ -2,7 +2,6 @@
 
 $(document).ready(function() {
     $("li").click(function(event) {
-        alert($(this).attr("id"));
         var sound = new Howl({src: ['./sounds/'+$(this).attr("id")+'.mp3']}).play();
     });
 });
